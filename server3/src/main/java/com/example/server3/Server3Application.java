33540a209d96ffd2b3455f18097fcdc4b3f9f224
xuxinchen <635736319@qq.com>
2021-01-11 09:46:31 +0800

    no message

--- server3/src/main/java/com/example/server3/Server3Application.java
+++ server3/src/main/java/com/example/server3/Server3Application.java
@@ -2,7 +2,6 @@ package com.example.server3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Server3Application
